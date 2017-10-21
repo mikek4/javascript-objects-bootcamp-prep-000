a321c1d5d87744a149e751b5fd739ef2e7f4af93
Michael Katz <mike.katz@outlook.com>
2017-10-21 19:03:44 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = { artist: 'song titles'};
+
+function updatePlaylist (playlist, artist, title){
+  return Object.assign({}, playlist, {[artist]: title})
+}
+
+function removeFromPlaylist (playlist, artist){
+  delete playlist.artist;
+  return playlist;
+
+}
